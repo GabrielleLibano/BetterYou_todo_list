827c6861f986fc7bd9fa47abf351fc54fa9fe227
GabrielleLibano <gabriellelibano18@gmail.com>
2024-09-01 17:05:02 -0300

    Mudança de linguagem - kotlin -> java

---
+++ BetterYou/src/main/java/com/example/BetterYou/BetterYouApplication.java
@@ -0,0 +1,13 @@
+package com.example.BetterYou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterYouApplication.class, args);
+	}
+
+}
---
+++ BetterYou/src/test/java/com/example/BetterYou/BetterYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BetterYou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
